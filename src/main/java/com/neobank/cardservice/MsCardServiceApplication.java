130c9e779e9490cecdb52cf8ebfbc2cf7f9ef2fc
Abbashatred <gitabbas26@gmail.com>
2026-05-30 23:12:19 +0400

    first commit

---
+++ src/main/java/com/neobank/cardservice/MsCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.neobank.cardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCardServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neobank/cardservice/MsCardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobank.cardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
